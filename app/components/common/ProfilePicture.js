c6060fe065a6a4f312b7d148c11a3d12af6d843c
Rob Calcroft <rob.calcroft@lloydsbanking.com>
2017-11-29 11:17:04 +0000

    Fixed profile picture breaking the site

--- app/components/common/ProfilePicture.js
+++ app/components/common/ProfilePicture.js
@@ -3,6 +3,9 @@ import md5 from 'md5';
 
 export default class ProfilePicture extends React.Component {
     render() {
+        if (!this.props.emailAddress) {
+          return null;
+        }
         return (
             <img className='circle responsive-img' src={`https://www.gravatar.com/avatar/${md5(this.props.emailAddress)}?d=mm&s=50`} alt='profile picture' />
         );
